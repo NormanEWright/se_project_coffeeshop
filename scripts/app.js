4f494bd8517de56585dfc00faeed30802e1edfde
= <n.wright.online@gmail.com>
2022-05-12 21:12:12 -0500

    Remove scripts

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-// Add placeholder text to number input field
-document.getElementById("guests").placeholder = "1-8";
-
-// Prevent default form submission
-document.querySelector(".form__button").addEventListener("click", function (event) {
-  event.preventDefault()
-});
